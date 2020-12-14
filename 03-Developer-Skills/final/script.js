b85fe592dc2a242c8e62e0c149bb2b7eb2e19718
milesdavis71 <OttosMops71>
2020-12-14 13:01:34 +0100

    8 Behind the scenes gyakorlás

--- 03-Developer-Skills/final/script.js
+++ 03-Developer-Skills/final/script.js
@@ -144,18 +144,19 @@ TEST DATA 2: [12, 5, -5, 0, 4]
 // - Add ... between elements and start and end of string
 // - Log string to console
 
-const data1 = [17, 21, 23];
-const data2 = [12, 5, -5, 0, 4];
 
 console.log(`... ${data1[0]}ºC ... ${data1[1]}ºC ... ${data1[2]}ºC ...`);
+
+*/
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
 
 const printForecast = function (arr) {
   let str = '';
   for (let i = 0; i < arr.length; i++) {
     str += `${arr[i]}ºC in ${i + 1} days ... `;
   }
+
   console.log('...' + str);
 };
 printForecast(data1);
-*/
-
