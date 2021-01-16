445d760f7008d57fb7ca338e68a9813ae8f8717e
milesdavis71 <OttosMops71>
2021-01-16 06:44:05 +0100

    9 szögletes zárójel, 11 Optional chaining

--- 09-Data-Structures-Operators/final/script_work.js
+++ 09-Data-Structures-Operators/final/script_work.js
@@ -1,14 +1,77 @@
-const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+// const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+// const openingHours = {
+//   [weekdays[3]]: {
+//     open: 12,
+//     close: 22,
+//   },
+//   [weekdays[4]]: {
+//     open: 11,
+//     close: 23,
+//   },
+//   [weekdays[5]]: {
+//     open: 0, // Open 24 hours
+//     close: 24,
+//   },
+// };
+
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+//   // ES6 enhanced object literals
+//   // openingHours,
+// };
+
+// const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+// const openingHours = {
+//   // a nap neve helyett a „weekdays” tömbből adjuk meg a megfelelő elemet
+//   // szögletes zárójelben.
+//   [weekdays[3]]: {
+//     open: 12,
+//     close: 22,
+//   },
+//   [weekdays[4]]: {
+//     open: 11,
+//     close: 23,
+//   },
+//   [weekdays[5]]: {
+//     open: 0,
+//     close: 24,
+//   },
+// };
+
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+//   //  ES6 szintaxis => külső objektum beágyazása
+//   openingHours,
+
+//   //  ES6 szintaxis => metódus írása
+//   order(startIndex, mainIndex) {
+//     return [this.starterMenu[startIndex], this.mainMenu[mainIndex]];
+//   },
+// };
+
+// console.log(restaurant.openingHours);
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 const openingHours = {
-  [weekdays[3]]: {
+  [days[3]]: {
     open: 12,
     close: 22,
   },
-  [weekdays[4]]: {
+  [days[4]]: {
     open: 11,
     close: 23,
   },
-  [weekdays[5]]: {
+  [days[5]]: {
     open: 0, // Open 24 hours
     close: 24,
   },
@@ -20,9 +83,13 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-  // ES6 enhanced object literals
   openingHours,
 };
 
-console.log(restaurant.openingHours.mo);
+console.log(restaurant.openingHours?.mon);
+
+for (const day of days) {
+  // console.log(day);
+  const open = restaurant.openingHours[day]?.open;
+  console.log(open);
+}
