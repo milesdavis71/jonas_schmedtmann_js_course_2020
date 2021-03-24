93f58abd6c9203ea4ace41951cf9e94d8277b190
milesdavis71 <OttosMops71>
2021-03-24 06:58:58 +0100

    11 challenge4 folyamatban

--- 11-Arrays-Bankist/final/script_11work2.js
+++ 11-Arrays-Bankist/final/script_11work2.js
@@ -204,3 +204,18 @@ btnSort.addEventListener('click', function (e) {
 
 const y = Array.from({ length: 7 }, () => 1);
 console.log(y);
+
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[1, 2, 3], [[4, 5], 6], 7, 8];
+console.log(arrDeep.flat(2));
+
+const overallBalance = accounts.flatMap(acc => acc.movements);
+console.log(overallBalance);
+
+// Callback függvény megírása külön
+const deposit = mov => mov > 0;
+console.log(accounts.movements.some(deposit));
+console.log(accounts.movements.every(deposit));
+console.log(accounts.movements.filter(deposit));
