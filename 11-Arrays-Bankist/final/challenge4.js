9a59041e525d3019df76d0ca6a78096df429f420
milesdavis71 <OttosMops71>
2021-03-26 07:01:04 +0100

    challenge4 gyakorlás

--- 11-Arrays-Bankist/final/challenge4.js
+++ 11-Arrays-Bankist/final/challenge4.js
@@ -44,13 +44,23 @@ console.log(dogs);
 const findSarah = dogs.find(dog => dog.owners.includes('Matilda'));
 console.log(findSarah);
 
-const ownersEatTooMuch = function (dog) {
-  dogs.map(dog => dog.curFood > dog.recommendedFood);
-};
-console.log(dogs.fore);
-// 3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch') and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
-// 4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
-// 5. Log to the console whether there is any dog eating EXACTLY the amount of food that is recommended (just true or false)
-// 6. Log to the console whether there is any dog eating an OKAY amount of food (just true or false)
-// 7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+const ownersEatTooMuch = dogs
+  .filter(dog => dog.curFood > dog.recommendedFood)
+  .flatMap(dog => dog.owners);
+console.log(ownersEatTooMuch);
+
+console.log(`${ownersEatTooMuch.join(' and ')} huhu huhu`);
+
+const ExactlyRecFood = dogs.some(dog => dog.curFood === dog.recommendedFood);
+console.log(ExactlyRecFood);
+
+const okayFood = dog =>
+  dog.curFood * 0.9 > dog.recommendedFood < dog.curFood * 1.1;
+console.log(dogs.some(okayFood));
+
+const okayDogs = dogs.filter(okayFood);
+console.log(okayDogs);
+
+const order = dogs.recommendedFood;
+console.log(dogs.recommendedFood);
 // 8. Create a shallow copy of the dogs array and sort it by recommended food portion in an ascending order (keep in mind that the portions are inside the array's objects)
