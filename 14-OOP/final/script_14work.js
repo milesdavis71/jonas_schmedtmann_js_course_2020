6abdf12ec7bffe7eb7082fdef0e1a2ac426aab3a
milesdavis71 <OttosMops71>
2021-04-15 13:10:15 +0200

    14 203 203 Constructor Functions and the new Operator gyakorlás

---
+++ 14-OOP/final/script_14work.js
@@ -0,0 +1,15 @@
+'strict';
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// Ezek történnek, ha egy függvényt a a new operátorral hívunk meg
+// 1. Létrejön egy új üres objektum -–> {}
+// 2. A new meghívja a Person függvényt, és a this-t beállítja az üres objektumra --> this = {}
+// 3. Az üres objektum {} hozzákapcsolódik a prototípusához
+// 4. A konstruktor visszaadja az először létrehozott üres objektumot, aminek most már nem kell üresnek lennie.
+
+
