e7203bcf76eb23c289631c6ec01c029ade4ee176
milesdavis71 <OttosMops71>
2021-06-15 08:25:01 +0200

    250 Handling Rejected Promises

--- 16-Asynchronous/final/script_16_work.js
+++ 16-Asynchronous/final/script_16_work.js
@@ -3,6 +3,29 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="${className}">
+  <img class="country__img" src="${data.flag}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)}</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>  
+  </div>
+</article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  // countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  // countriesContainer.style.opacity = 1;
+};
+
 ////////////////////////////////
 /*
 const getCountryData = function (country) {
@@ -35,55 +58,70 @@ getCountryData('hungary');
 getCountryData('germany');
 */
 
-/*
-const renderCountry = function (data, className = '') {
-  const html = `
-  <article class="${className}">
-  <img class="country__img" src="${data.flag}" />
-  <div class="country__data">
-    <h3 class="country__name">${data.name}</h3>
-    <h4 class="country__region">${data.region}</h4>
-    <p class="country__row"><span>👫</span>${(
-      +data.population / 1000000
-    ).toFixed(1)}</p>
-    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>  
-  </div>
-</article>`;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
+// const getCountryAndNeighbour = function (country) {
+//   // AJAX call country
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+//   request.send();
 
-const getCountryAndNeighbour = function (country) {
-  // AJAX call country
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
-  request.send();
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     // Render country 1
+//     renderCountry(data);
 
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    // Render country 1
-    renderCountry(data);
+//     // Get neighbour country 2 (neighbour)
+//     const [neighbour] = data.borders;
+//     if (!neighbour) return;
 
-    // Get neighbour country 2 (neighbour)
-    const [neighbour] = data.borders;
-    if (!neighbour) return;
+//     // AJAX call country 2 (neighbour)
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+//     request2.send();
 
-    // AJAX call country 2 (neighbour)
-    const request2 = new XMLHttpRequest();
-    request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbour}`);
-    request2.send();
+//     request2.addEventListener('load', function () {
+//       const data2 = JSON.parse(this.responseText);
+//       renderCountry(data2, 'neighbour');
+//     });
+//   });
+// };
+// getCountryAndNeighbour('portugal');
 
-    request2.addEventListener('load', function () {
-      const data2 = JSON.parse(this.responseText);
-      renderCountry(data2, 'neighbour');
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+// getCountryData('hungary');
+
+const getCountryData = function (country) {
+  // Country 1
+  fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'))
+    .catch(err => {
+      console.error(err);
+      renderError(`Valami nem jó. ${err.message} Próbáld meg újra.`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
     });
-  });
 };
-getCountryAndNeighbour('portugal');
-*/
-
-const request = fetch('https://restcountries.eu/rest/v2/name/portugal');
-console.log(request);
-
-const getCountryData = function (country) {};
+btn.addEventListener('click', function () {
+  getCountryData('portugal');
+});
+// const getCountryData = function (country) {};
