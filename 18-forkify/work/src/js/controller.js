7efc3f5bbc28a53f748ec294409366497057730a
milesdavis71 <OttosMops71>
2021-08-09 14:56:29 +0200

    search folyamatban.

--- 18-forkify/work/src/js/controller.js
+++ 18-forkify/work/src/js/controller.js
@@ -1,6 +1,8 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
@@ -19,6 +21,8 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
     // 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -27,7 +31,7 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render search results
-    console.log(model.state.search.results);
+    searchView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
